8e0640a451a4fe481817789805a53e72892e687f
Frantisek Novak <frantisek.novak@atomsoftwarestudios.com>
2017-04-01 00:20:59 +0200

    Added comments

--- ATsDoc/test/classes/classes-inheritance.ts
+++ ATsDoc/test/classes/classes-inheritance.ts
@@ -1,13 +1,33 @@
-﻿namespace classes_inheritance {
+﻿/**
+ * This is a short description of the classes_inheritance namespace
+ * This is a long description of the classes_inheritance namespace
+ */
+namespace classes_inheritance {
 
+   /**
+    * This is a short description of the test namespace
+    * This is a long description of the test namespace
+    */
     namespace test {
 
+        /**
+         * This is a short description of the IA interface
+         * This is a long description of the IA interface
+         */
         export interface IA {
         }
 
+        /**
+         * This is a short description of the IB interface
+         * This is a long description of the IB interface
+         */
         export interface IB {
         }
 
+        /**
+         * This is a short description of the A class
+         * This is a long description of the A class
+         */
         export class A {
         }
 
@@ -15,13 +35,39 @@
 
     import t = test;
 
-    class B {
+    /**
+     * This is a short description of the B class
+     * This is a long description of the B class
+     */
+    class B extends test.A {
     }
 
-    /*class C extends t.A implements t.IA, t.IB {
-    }*/
-
+    /**
+     * This is a short description of the C class
+     * This is a long description of the C class
+     */
     class C extends B {
+    }
+
+    /**
+     * This is a short description of the D class
+     * This is a long description of the D class
+     */
+    class D extends t.A implements t.IA, t.IB {
+    }
+
+    /**
+     * This is a short description of the E class
+     * This is a long description of the E class
+     */
+    class E {
+    }
+
+    /**
+     * This is a short description of the F class
+     * This is a long description of the F class
+     */
+    class F extends E {
     }
 
 }
