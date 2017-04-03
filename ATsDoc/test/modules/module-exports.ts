f0976925610be1facf2e2aadfc6e47b6e7b989ae
Frantisek Novak <frantisek.novak@atomsoftwarestudios.com>
2017-04-04 00:11:35 +0200

    Added documentation

--- ATsDoc/test/modules/module-exports.ts
+++ ATsDoc/test/modules/module-exports.ts
@@ -1,7 +1,19 @@
-﻿export namespace module_commonjs {
+﻿/**
+ * Exported namespace module_commonjs
+ * module_commonjs Long
+ */
+export namespace module_commonjs {
 
+    /**
+     * Exported variable a
+     * variable a long
+     */
     export let a: number = 1;
 
 }
 
+/**
+ * Exported variable b
+ * variable b long
+ */
 export let b: string = "a";
